219864a3be476b399b507b218f317822ff49c60d
avezzu <avezzu@ethz.ch>
2023-10-03 13:47:27 +0200

    adjusted routing for github page

--- docs/assets/loader.js
+++
@@ -1,7 +0,0 @@
-function preloader(){
-
-    window.addEventListener("load", () => {
-        document.querySelector(".loader").classList.add("loader--hidden");
-      })
-    console.log("hello")
-}
--- docs/scripts.c6a1a6cfbdb222ad.js
+++
@@ -1,1 +0,0 @@
-function preloader(){window.addEventListener("load",()=>{document.querySelector(".loader").classList.add("loader--hidden")}),console.log("hello")}
